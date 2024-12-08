4d723c09ab03b210110ff77d87a9593dfcb7de78
khanh123bo123 <pipizxc05@gmail.com>
2024-12-09 01:49:04 +0700

    update

--- js/admin.js
+++ js/admin.js
@@ -13,37 +13,26 @@ function load() {
 if (localStorage.getItem("listProduct") != null) {
     load();
 }
-var productAdmin = function (category = null) {
-    document.getElementById("product-admin").innerHTML = "";
-    for (var i in product) {
-        var data = JSON.parse(JSON.stringify(product[i]));
-        if (category && data.category !== category) {
-            continue;
-        }
-        var listproduct1 = "<tr>";
-        listproduct1 += "<td>" + data.id + "</td>";
-        listproduct1 += "<td>" + data.name + "</td>";
-        listproduct1 +=
-            '<td><img src="../img/' +
-            data.img +
-            '" alt="" style="width: 50px;"></td>';
-        listproduct1 += "<td>" + data.price + "</td>";
-        listproduct1 += "<td>" + data.category + "</td>";
-        //  listproduct1+='';
-        listproduct1 +=
-            '<td><button onclick="updateProduct(' +
-            i +
-            ')" class="btn btn-outline-danger"  data-toggle="modal" data-target="#updateProduct"><i class="fas fa-cogs"></i></button>';
-        listproduct1 +=
-            '<button onclick="deleteProduct(' +
-            i +
-            ')" class="btn ml-1 btn-outline-warning"><i class="fas fa-trash"></i></button></td>';
-        listproduct1 += "</tr>";
+var productAdmin = function(){
+    var listproduct1 ="";
+    for (var i in product){
+        var data = JSON.parse(JSON.stringify(product[i]))
+     var listproduct1 = '<tr>';
+      listproduct1+='<td>'+data.id+'</td>';
+      listproduct1+='<td>'+data.name+'</td>';
+      listproduct1+='<td><img src="../img/'+data.img+'" alt="" style="width: 50px;"></td>';
+      listproduct1+='<td>'+data.price+'</td>';
+     //  listproduct1+='';
+      listproduct1+='<td><button onclick="updateProduct('
+      +i+')" class="btn btn-outline-danger"  data-toggle="modal" data-target="#updateProduct"><i class="fas fa-cogs"></i></button>';
+      listproduct1+='<button onclick="deleteProduct('
+      +i+')" class="btn ml-1 btn-outline-warning"><i class="fas fa-trash"></i></button></td>';
+      listproduct1+='</tr>';  
 
-        document.getElementById("product-admin").innerHTML += listproduct1;
+     document.getElementById("product-admin").innerHTML += listproduct1;
     }
     // Save();
-};
+    }
 
 var addProduct = function () {
     var Product = {
@@ -51,7 +40,7 @@ var addProduct = function () {
         name: document.getElementById("name").value,
         img: document.getElementById("img").value,
         price: document.getElementById("price").value,
-        category: document.getElementById("category").value,
+        
     };
     product.push(Product);
     localStorage.setItem("listProduct", JSON.stringify(product));
@@ -72,7 +61,7 @@ var updateProduct = function (i) {
     document.getElementById("named").value = k.name;
     document.getElementById("imgd").value = k.img;
     document.getElementById("priced").value = k.price;
-    document.getElementById("categoryd").value = k.category;
+
     document.getElementById("idd").setAttribute("disabled", "disabled");
     document.getElementById("submitUpdate").innerHTML =
         '<button class="btn btn-outline-danger mt-3" onclick="submitUpdate(' +
@@ -85,16 +74,16 @@ var submitUpdate = function (i) {
     k.name = document.getElementById("named").value;
     k.img = document.getElementById("imgd").value;
     k.price = document.getElementById("priced").value;
-    k.category = document.getElementById("categoryd").value;
+    
     // document.getElementById("idd").setAttribute("disabled","disabled");
     localStorage.setItem("listProduct", JSON.stringify(product));
     window.location.reload();
 };
 
 // Chức năng lọc sản phẩm theo danh mục
-function filterProducts(category) {
-    productAdmin(category);
-}
+// function filterProducts(category) {
+//     productAdmin(category);
+// }
 
 var userAdmin = function () {
     var listproduct = "";
